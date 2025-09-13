5f1b70de75df0ab90e2ec6c0db82c052601f2c7a
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2025-09-13 19:04:39 +0500

    Finished on Todo, the previous version of this Todo was in html and css form. Now Todo is ready with JS!

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,27 @@
 const ulEl = document.querySelector('ul')
-const liEl = document.querySelector('li')
+const liEl = document.querySelectorAll('li')
 const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+	// ulEl.innerHTML += "<li>Yangi li element qo'shildi😊</li>"
+	const li = document.createElement('li')
+	li.textContent = "Yangi li element qo'shildi😊"
+	ulEl.appendChild(li)
+})
+
+liEl.forEach(item => {
+	item.addEventListener('click', e => {
+		// e.target.style.textDecoration = 'line-through'
+		// e.target.style.opacity = '0.7'
+		// e.target.remove()
+		// console.log('Li')
+		// e.stopPropagation()
+	})
+})
+
+ulEl.addEventListener('click', e => {
+	// console.log('Ul')
+	if (e.target.nodeName == 'LI') {
+		e.target.remove()
+	}
+})
