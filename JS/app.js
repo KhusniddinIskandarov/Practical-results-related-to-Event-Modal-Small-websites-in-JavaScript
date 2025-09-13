732c63f257ce6370fe6e1f10d9f87cf347b18dca
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2025-09-13 20:34:52 +0500

    Forms start

--- JS/app.js
+++ JS/app.js
@@ -1,25 +1,2 @@
-const modal = document.getElementById('modal')
-const showBtn = document.getElementById('show-btn')
-const closeBtn = document.getElementById('close-btn')
-const overlay = document.getElementById('overlay')
-
-const addFunc = () => {
-	modal.classList.add('hidden')
-	overlay.classList.add('hidden')
-}
-
-const removeFunc = () => {
-	modal.classList.remove('hidden')
-	overlay.classList.remove('hidden')
-}
-
-showBtn.addEventListener('click', removeFunc)
-closeBtn.addEventListener('click', addFunc)
-overlay.addEventListener('click', addFunc)
-
-document.addEventListener('keydown', e => {
-	if (e.key == 'Escape') {
-		modal.classList.add('hidden')
-		overlay.classList.add('hidden')
-	}
-})
+const signupForm = document.getElementById('signup-form')
+const input = document.getElementById('username')
